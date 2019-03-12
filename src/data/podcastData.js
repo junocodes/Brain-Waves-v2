4a278a1d12e7730cbd03a9ca94ba146cfebfb6c4
Justin Juno <hello@juno.codes>
2019-03-11 20:48:46 -0500

    add podcast assets

---
+++ src/data/podcastData.js
@@ -0,0 +1,120 @@
+export default [
+  {
+    title: "Culture",
+    coverArt: "/assets/images/covers/culture.png",
+    slug: "culture",
+    episodes: [
+      {
+        title: "Culture 101",
+        host: "Bessie Auden",
+        duration: "161.71",
+        audioSrc: "/assets/podcasts/blue.mp3"
+      },
+      {
+        title: "Building Better Culture",
+        host: "Callie Lennon",
+        duration: "103.96",
+        audioSrc: "/assets/podcasts/green.mp3"
+      },
+      {
+        title: "It's More Than Beer",
+        host: "Byron Murray",
+        duration: "268.45",
+        audioSrc: "/assets/podcasts/red.mp3"
+      }
+    ]
+  },
+  {
+    title: "Diversity",
+    coverArt: "/assets/images/covers/diversity.png",
+    slug: "diversity",
+    episodes: [
+      {
+        title: "Diversity 101",
+        host: "Bessie Auden",
+        duration: "161.71",
+        audioSrc: "/assets/podcasts/blue.mp3"
+      },
+      {
+        title: "Building Diverse Teams",
+        host: "Waldo Wren",
+        duration: "103.96",
+        audioSrc: "/assets/podcasts/green.mp3"
+      },
+      {
+        title: "Diversity Is The Issue",
+        host: "Suzette Inez",
+        duration: "268.45",
+        audioSrc: "/assets/podcasts/red.mp3"
+      },
+      {
+        title: "Diversity Always Wins",
+        host: "Zola Farrah",
+        duration: "153.14",
+        audioSrc: "/assets/podcasts/pink.mp3"
+      }
+    ]
+  },
+  {
+    title: "Leadership",
+    coverArt: "/assets/images/covers/leadership.png",
+    slug: "leadership",
+    episodes: [
+      {
+        title: "Leadership 101",
+        host: "Bessie Auden",
+        duration: "161.71",
+        audioSrc: "/assets/podcasts/blue.mp3"
+      },
+      {
+        title: "Becoming A Leader",
+        host: "Zola Farrah",
+        duration: "103.96",
+        audioSrc: "/assets/podcasts/green.mp3"
+      },
+      {
+        title: "Stop Being Bossy",
+        host: "Suzette Inez",
+        duration: "268.45",
+        audioSrc: "/assets/podcasts/red.mp3"
+      },
+      {
+        title: "Empowering Teams To Scale",
+        host: "Byron Murray",
+        duration: "153.14",
+        audioSrc: "/assets/podcasts/pink.mp3"
+      },
+      {
+        title: "Learning Leaders",
+        host: "Nico Orsen",
+        duration: "374.22",
+        audioSrc: "/assets/podcasts/magenta.mp3"
+      }
+    ]
+  },
+  {
+    title: "Teams",
+    coverArt: "/assets/images/covers/teams.png",
+    slug: "teams",
+    episodes: [
+      {
+        title: "Teams 101",
+        host: "Bessie Auden",
+        duration: "161.71",
+        audioSrc: "/assets/podcasts/blue.mp3"
+      },
+      {
+        title: "Everyone Is A Team Player",
+        host: "Nico Orsen",
+        duration: "153.14",
+        audioSrc: "/assets/podcasts/pink.mp3"
+      },
+      {
+        title: "Get Off The Bench",
+        host: "Zane Roman",
+        duration: "374.22",
+        audioSrc: "/assets/podcasts/magenta.mp3"
+      }
+    ]
+  }
+];
